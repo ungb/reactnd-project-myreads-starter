46e51ccc7314b1d04928e74be56ef351f0918725
Bryant Ung <bryant.ung@outlook.com>
2018-07-09 22:46:47 -0700

    bug: remove console.log

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ class BooksApp extends React.Component {
   }
 
   render() {
-    console.log(this.state)
     return (
       <div className="app">
           <Route exact path="/" render={() => (
